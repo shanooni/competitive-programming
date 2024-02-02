e1a2f581b7da6b3351cad51f7f59e53cc9eb7442
Shanoon Issaka <133187982+shanooni@users.noreply.github.com>
2024-02-02 19:54:42 +0000

    arithmetic operation function

---
+++ arithmetic_operator.py
@@ -0,0 +1,28 @@
+"""
+Task
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+
+"""
+
+
+def arithmetic_operation(a, b):
+    add = a + b
+    sub = a - b
+    product = a * b
+    print(add)
+    print(sub)
+    print(product)
+
+
+arithmetic_operation(3, 5)
+
+"""
+output: 
+8
+-2
+15
+"""
