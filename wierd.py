66a02e6f86c18c6d34333a362a74061e879bbfa4
Shanoon Issaka <133187982+shanooni@users.noreply.github.com>
2024-02-03 11:56:45 +0000

    even check for wierd output

---
+++ wierd.py
@@ -0,0 +1,24 @@
+"""
+Task
+Given an integer n , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+"""
+
+
+def wierd(n: int):
+    if n % 2 != 0:
+        print("Weird")
+    else:
+        if 2 <= n <= 5:
+            print("Not Weird")
+        elif 6 <= n <= 20:
+            print("Weird")
+        elif n > 20:
+            print(" Not Weird")
+
+
+wierd(4)
