650b2206cf77479d736b8dd79bf2fcc6ae76e10c
Shanoon Issaka <133187982+shanooni@users.noreply.github.com>
2024-02-05 16:22:09 +0000

    hackerrank. python-arithmetic-operators

--- arithmetic_operator.py
+++ arithmetic_operator.py
@@ -8,12 +8,10 @@ The third line contains the product of the two numbers.
 
 """
 
-
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
 
     print(a + b)
-    print(a -b)
+    print(a - b)
     print(a * b)
-
