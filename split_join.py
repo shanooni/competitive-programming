84a3ca888571bc65eb7c70253908796903b32b29
Shanoon Issaka <133187982+shanooni@users.noreply.github.com>
2024-02-05 16:30:41 +0000

    hackerrank. python-string-split-and-join

--- split_join.py
+++ split_join.py
@@ -4,16 +4,12 @@ You are given a string. Split the string on a " " (space) delimiter and join usi
 """
 
 
-def split_join(s: str):
-    split_and_join = s.split(" ")
-    split_and_join = "-".join(split_and_join)
-    return split_and_join
+def split_and_join(line: str) -> str:
+    split = line.split(" ")
+    join = "-".join(split)
+    return join
 
 
-print(split_join("this is a string"))
-
-
-"""
-output
-this-is-a-string
-"""
+if __name__ == '__main__':
+    line = input()
+    print(split_and_join(line))
