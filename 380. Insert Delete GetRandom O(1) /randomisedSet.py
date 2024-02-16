135b4678703e1e018ceffae750c5b5be8a52959d
Shanoon Issaka <133187982+shanooni@users.noreply.github.com>
2024-02-16 21:03:44 +0000

    leetcode 380 Insert Delete GetRandom

---
+++ 380. Insert Delete GetRandom O(1) /randomisedSet.py
@@ -0,0 +1,19 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.randomList = []
+
+    def insert(self, val: int) -> bool:
+        if val not in self.randomset:
+            self.randomset.append(val)
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        if val in self.randomset:
+            self.randomset.remove(val)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        return random.choice(self.randomset)
